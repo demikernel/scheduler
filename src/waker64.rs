2e51f07eb7850176d86406de198b52eb537e9af5
Pedro Henrique Penna <ppenna@microsoft.com>
2022-02-09 02:52:11 -0800

    [waker64] Enhancement: Updating API doc for Waker64

--- src/waker64.rs
+++ src/waker64.rs
@@ -20,27 +20,27 @@ pub struct Waker64(UnsafeCell<u64>);
 
 /// Associate Functions for 64-Bit Wakers
 impl Waker64 {
-    /// Creates a [WakerU64] from a value.
+    /// Creates a 64-Bit Waker from `val`.
     pub fn new(val: u64) -> Self {
         Waker64(UnsafeCell::new(val))
     }
 
-    /// Applies the OR operator between `val` and the target [WakerU64].
-    /// The resulting value is stored back in the target [WakerU64].
+    /// Applies the OR operator between `val` and the target [Waker64].
+    /// The resulting value is stored back in the target [Waker64].
     pub fn fetch_or(&self, val: u64) {
         let s = unsafe { &mut *self.0.get() };
         *s |= val;
     }
 
-    /// Applies the AND operator between `val` and the target [WakerU64].
-    /// The resulting value is stored back in the target [WakerU64].
+    /// Applies the AND operator between `val` and the target [Waker64].
+    /// The resulting value is stored back in the target [Waker64].
     pub fn fetch_and(&self, val: u64) {
         let s = unsafe { &mut *self.0.get() };
         *s &= val;
     }
 
-    /// Applies the ADD operator between `val` and the target [WakerU64].
-    /// The resulting value is stored back in the target [WakerU64] and the old
+    /// Applies the ADD operator between `val` and the target [Waker64].
+    /// The resulting value is stored back in the target [Waker64] and the old
     /// value is returned.
     pub fn fetch_add(&self, val: u64) -> u64 {
         let s = unsafe { &mut *self.0.get() };
@@ -49,8 +49,8 @@ impl Waker64 {
         old
     }
 
-    /// Applies the SUB operator between `val` and the target [WakerU64].
-    /// The resulting value is stored back in the target [WakerU64] and the old
+    /// Applies the SUB operator between `val` and the target [Waker64].
+    /// The resulting value is stored back in the target [Waker64] and the old
     /// value is returned.
     pub fn fetch_sub(&self, val: u64) -> u64 {
         let s = unsafe { &mut *self.0.get() };
@@ -59,13 +59,13 @@ impl Waker64 {
         old
     }
 
-    /// Returns the value stored in the the target [WakerU64].
+    /// Returns the value stored in the the target [Waker64].
     pub fn load(&self) -> u64 {
         let s = unsafe { &mut *self.0.get() };
         *s
     }
 
-    /// Replaces the value stored in the the target [WakerU64] by `val`.
+    /// Replaces the value stored in the the target [Waker64] by `val`.
     pub fn swap(&self, val: u64) -> u64 {
         let s = unsafe { &mut *self.0.get() };
         mem::replace(s, val)
