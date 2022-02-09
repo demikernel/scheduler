cdfb41e86bbe8036866664054bd0e4007c200f6f
Pedro Henrique Penna <ppenna@microsoft.com>
2022-02-09 05:22:09 -0800

    [page] Enhancement: API Doc for WakerRef

--- src/page/waker_ref.rs
+++ src/page/waker_ref.rs
@@ -1,16 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-//! A "page" is made up of 64 contiguous entries, each entry represents the state of a task/future
-//! in our scheduler. This page is represented by a 64-bit integer where the ith bit corresponds to
-//! the ith task in that page. This way fast bit arithmetic can be used to index into a task's
-//! state and uniquely identify a task among multiple pages.
-
 //==============================================================================
 // Imports
 //==============================================================================
 
-use super::{page::WakerPage, WakerPageRef};
+use super::{page::WakerPage, WakerPageRef, WAKER_PAGE_SIZE};
 use ::std::{
     mem,
     ptr::NonNull,
@@ -21,6 +16,10 @@ use ::std::{
 // Structures
 //==============================================================================
 
+/// Waker Reference
+///
+/// This reference is a representation for the status of a particular task
+/// stored in a [WakerPage].
 #[repr(transparent)]
 pub struct WakerRef(NonNull<u8>);
 
@@ -28,30 +27,36 @@ pub struct WakerRef(NonNull<u8>);
 // Associate Functions
 //==============================================================================
 
+/// Associate Functions for Waker References
 impl WakerRef {
     pub fn new(raw_page_ref: NonNull<u8>) -> Self {
-        Self { 0: raw_page_ref }
+        Self(raw_page_ref)
     }
 
+    /// Casts the target [WakerRef] back into reference to a [WakerPage] plus an
+    /// offset indicating the target task in the latter structure.
+    ///
+    /// For more information on this hack see comments on [crate::page::WakerPageRef].
     fn base_ptr(&self) -> (NonNull<WakerPage>, usize) {
-        let ptr = self.0.as_ptr();
-
-        let forward_offset = ptr.align_offset(64);
-        let mut base_ptr = ptr;
-        let mut offset = 0;
+        let ptr: *mut u8 = self.0.as_ptr();
+        let forward_offset: usize = ptr.align_offset(WAKER_PAGE_SIZE);
+        let mut base_ptr: *mut u8 = ptr;
+        let mut offset: usize = 0;
         if forward_offset != 0 {
-            offset = 64 - forward_offset;
+            offset = WAKER_PAGE_SIZE - forward_offset;
             base_ptr = ptr.wrapping_sub(offset);
         }
         unsafe { (NonNull::new_unchecked(base_ptr).cast(), offset) }
     }
 
+    /// Sets the notification flag for the task that associated with the target [WakerRef].
     fn wake_by_ref(&self) {
-        let (base_ptr, offset) = self.base_ptr();
-        let base = unsafe { &*base_ptr.as_ptr() };
-        base.notify(offset);
+        let (base_ptr, ix): (NonNull<WakerPage>, usize) = self.base_ptr();
+        let base: &WakerPage = unsafe { &*base_ptr.as_ptr() };
+        base.notify(ix);
     }
 
+    /// Sets the notification flag for the task that is associated with the target [WakerRef].
     fn wake(self) {
         self.wake_by_ref()
     }
@@ -61,63 +66,20 @@ impl WakerRef {
 // Trait Implementations
 //==============================================================================
 
-impl Into<RawWaker> for WakerRef {
-    fn into(self) -> RawWaker {
-        let ptr = self.0.cast().as_ptr() as *const ();
-        let waker = RawWaker::new(ptr, &VTABLE);
-        mem::forget(self);
-        waker
-    }
-}
-
-// The following methods are used to implement Waker for WakeRef.
-//
-// While it may look complicated it is just doing standard implementation of the Waker vtable as
-// required by Rust.
-//
-// Ultimately, calling .wake() for our waker just calls [WakerPage::notify] which sets the
-// appropriate bit to 1.
-
-unsafe fn waker_ref_clone(ptr: *const ()) -> RawWaker {
-    let p = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
-    let q = p.clone();
-    mem::forget(p);
-    q.into()
-}
-
-unsafe fn waker_ref_wake(ptr: *const ()) {
-    let p = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
-    p.wake();
-}
-
-unsafe fn waker_ref_wake_by_ref(ptr: *const ()) {
-    let p = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
-    p.wake_by_ref();
-    mem::forget(p);
-}
-
-unsafe fn waker_ref_drop(ptr: *const ()) {
-    let p = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
-    drop(p);
-}
-
-pub const VTABLE: RawWakerVTable = RawWakerVTable::new(
-    waker_ref_clone,
-    waker_ref_wake,
-    waker_ref_wake_by_ref,
-    waker_ref_drop,
-);
-
+/// Clone Trait Implementation for Waker References
 impl Clone for WakerRef {
     fn clone(&self) -> Self {
-        let (base_ptr, _) = self.base_ptr();
-        let p = WakerPageRef::new(base_ptr);
+        let (base_ptr, _): (NonNull<WakerPage>, _) = self.base_ptr();
+        let p: WakerPageRef = WakerPageRef::new(base_ptr);
+        // Increment reference count.
         mem::forget(p.clone());
+        // This is not a double increment.
         mem::forget(p);
         WakerRef(self.0)
     }
 }
 
+/// Drop Trait Implementation for Waker References
 impl Drop for WakerRef {
     fn drop(&mut self) {
         let (base_ptr, _) = self.base_ptr();
@@ -125,6 +87,55 @@ impl Drop for WakerRef {
         drop(WakerPageRef::new(base_ptr));
     }
 }
+
+/// Convert Trait Implementation for Waker References
+impl Into<RawWaker> for WakerRef {
+    fn into(self) -> RawWaker {
+        let ptr: *const () = self.0.cast().as_ptr() as *const ();
+        let waker: RawWaker = RawWaker::new(ptr, &VTABLE);
+        // Increment reference count.
+        mem::forget(self);
+        waker
+    }
+}
+
+/// Clones the task that is associated to the target [WakerRef].
+unsafe fn waker_ref_clone(ptr: *const ()) -> RawWaker {
+    let p: WakerRef = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
+    let q: WakerRef = p.clone();
+    // Increment reference count.
+    mem::forget(p);
+    q.into()
+}
+
+/// Wakes up the task that is associated to the target [WakerRef].
+unsafe fn waker_ref_wake(ptr: *const ()) {
+    let p = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
+    p.wake();
+}
+
+/// Wakes up the task that is associated to the target [WakerRef].
+unsafe fn waker_ref_wake_by_ref(ptr: *const ()) {
+    let p: WakerRef = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
+    p.wake_by_ref();
+    // Increment reference count.
+    mem::forget(p);
+}
+
+/// Drops the task that is associated to the target [WakerRef].
+unsafe fn waker_ref_drop(ptr: *const ()) {
+    let p: WakerRef = WakerRef(NonNull::new_unchecked(ptr as *const u8 as *mut u8));
+    // Decrement reference count.
+    drop(p);
+}
+
+/// Raw Waker Trait Implementation for Waker References
+pub const VTABLE: RawWakerVTable = RawWakerVTable::new(
+    waker_ref_clone,
+    waker_ref_wake,
+    waker_ref_wake_by_ref,
+    waker_ref_drop,
+);
 
 //==============================================================================
 // Unit Tests
